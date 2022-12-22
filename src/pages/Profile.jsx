35ee786618b926c779472b4c5b1f16cb36ee81c8
copsgit <github@copsking.com>
2022-12-22 03:28:24 -0500

    profile info

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,37 +1,48 @@
-import React, { useState, useEffect } from 'react'
-
-import { DisplayCampaigns } from '../components';
-import { useStateContext } from '../context'
+import React, {useState, useEffect} from 'react'
+import {thirdweb} from '../assets';
+import {DisplayCampaigns} from '../components';
+import {useStateContext} from '../context'
 
 const Profile = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [campaigns, setCampaigns] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [campaigns, setCampaigns] = useState([]);
 
-  const { address, contract, getUserCampaigns } = useStateContext();
+    const {address, contract, getUserCampaigns} = useStateContext();
 
-  const fetchCampaigns = async () => {
-    setIsLoading(true);
-    const data = await getUserCampaigns();
-    setCampaigns(data);
-    setIsLoading(false);
-  }
+    console.log(campaigns)
+    const fetchCampaigns = async () => {
+        setIsLoading(true);
+        const data = await getUserCampaigns();
+        setCampaigns(data);
+        setIsLoading(false);
+    }
 
-  useEffect(() => {
-    if(contract) fetchCampaigns().then();
-  }, [address, contract]);
+    useEffect(() => {
+        if (contract) fetchCampaigns().then();
+    }, [address, contract]);
 
-  return (
-      <div>
-        <div className="flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
-
+    const totalAmountRaised = campaigns.reduce((acc, curr) => parseFloat(acc.amountCollected) + parseFloat(curr.amountCollected));
+    const totalAmountTargeted = campaigns.reduce((acc, curr) => parseFloat(acc.target) + parseFloat(curr.target));
+    return (
+        <div>
+            <div className="flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+                <div className="flex flex-col items-center gap-4">
+                    <div className="w-[100px] h-[100px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+                        <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain"/>
+                    </div>
+                    <p className="text-[#fff] text-[16px] font-epilogue font-semibold leading-[18px]">{address}</p>
+                    <p className="text-[#fff] text-[16px] font-epilogue font-semibold leading-[18px]">
+                        Total amount raised: {totalAmountRaised} ETH / {totalAmountTargeted} ETH ({(totalAmountRaised / totalAmountTargeted).toFixed(3) * 100}%)
+                    </p>
+                </div>
+            </div>
+            <DisplayCampaigns
+                title="All Campaigns"
+                isLoading={isLoading}
+                campaigns={campaigns}
+            />
         </div>
-        <DisplayCampaigns
-            title="All Campaigns"
-            isLoading={isLoading}
-            campaigns={campaigns}
-        />
-      </div>
-  )
+    )
 }
 
 export default Profile
